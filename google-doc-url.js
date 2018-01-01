d7558c181a42574bbe4a3841b1dad932292a102c
Dusan <jucabika@hotmail.com>
2018-01-01 15:12:19 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/16Oz8Sg6YYZ8EiPEU0j-rXx1JriZQlfgsH1rx298LXYE/edit#gid=0';
